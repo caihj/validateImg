ccca68997fe16c34d51ce676f69f52d2edc5ddf7
caihj <875250682@qq.com>
2017-01-24 17:10:51 +0800

    first

---
+++ getImg.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+import wget
+import uuid
+
+def getImg(url,savePath):
+    print 'get',url
+    saveName=savePath+'/'+str(uuid.uuid1())+'.jpg'
+    print 'save as',saveName
+    wget.download(url,saveName)
+
+
+if __name__=='__main__':
+    count=1000
+    for i in range(0,count):
+        print 'get ',i
+        getImg('http://page2.dfpan.com/verifyimg/getPcv.html','C:/python_workspace/tools/validateImg/img')
