b9a79f7fc373e1d6fc3f86a6c558e1c42e05b818
caihj <875250682@qq.com>
2017-02-06 15:51:46 +0800

    提交一部分验证代码

---
+++ 3.py
@@ -0,0 +1,78 @@
+#coding:utf-8
+
+import numpy as np
+import os
+from PIL import Image
+
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)*9
+    return 1/(1+np.exp(-x))
+
+
+np.random.seed(1)
+
+# randomly initialize our weights with mean 0
+syn0 = np.random.random((165,10))
+syn1 = np.random.random((10,1))
+
+def train(X,y):
+    global syn0
+    global syn1
+
+    # Feed forward through layers 0, 1, and 2
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+    l2 = nonlin(np.dot(l1,syn1))
+
+    # how much did we miss the target value?
+    l2_error = y - l2
+
+    # in what direction is the target value?
+    # were we really sure? if so, don't change too much.
+    l2_delta = l2_error*nonlin(l2,deriv=True)
+
+    # how much did each l1 value contribute to the l2 error (according to the weights)?
+    l1_error = l2_delta.dot(syn1.T)
+
+    # in what direction is the target l1?
+    # were we really sure? if so, don't change too much.
+    l1_delta = l1_error * nonlin(l1,deriv=True)
+
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += l0.T.dot(l1_delta)
+
+def match(d):
+    global syn0
+    global syn1
+    l1=nonlin(np.dot(d,syn0))
+    l2=nonlin(np.dot(l1,syn1))
+    return l2
+
+def main():
+    images=os.listdir('ch')
+    i=0
+    for img in images:
+        print " img name",img
+        im = Image.open("ch/"+img)
+        arr = np.asarray(im,dtype="uint32")  #将img数据转化为数组形式
+        l = []
+        for ar in arr:
+            for a in ar:
+                l.append(a)
+
+        label=img.split('.')[1]
+        train(np.asanyarray([l]),np.asanyarray([[int(label)]]))
+
+
+if __name__=='__main__':
+    main()
+
+    im=Image.open("ch/58f3a340-ea89-11e6-9d29-448a5b698537.8.png")
+    arr = np.asarray(im,dtype="float32")  #将img数据转化为数组形式
+    l = []
+    for ar in arr:
+        for a in ar:
+            l.append(a)
+
+    print match(np.asanyarray(l))
--- traning.py
+++ traning.py
@@ -3,10 +3,8 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.advanced_activations import PReLU
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD, Adadelta, Adagrad
 from keras.utils import np_utils, generic_utils
